d7ea95752a57f43768c10f2242f7a51fa39e0027
hey <103824094+mushroomhater07@users.noreply.github.com>
2023-05-17 02:43:02 +0100

    add

---
+++ itemparty/src/main/java/com/hheylau/blockparty/Blockparty.java
@@ -0,0 +1,29 @@
+package com.hheylau.blockparty;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Blockparty extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this,this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void blockbreak(BlockBreakEvent event){
+        Player pl = event.getPlayer();
+        pl.sendMessage(event.getBlock().getBlockData().getAsString());
+    }
+
+
+}
